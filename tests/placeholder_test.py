314c3c31224e8ce0364614c7d7db7ec9c2fce171
Simone Porreca <porrecasimone@gmail.com>
2024-02-07 20:53:34 +0100

    Create placeholder_test.py

---
+++ tests/placeholder_test.py
@@ -0,0 +1,15 @@
+"""
+The module is intended to hold a placeholder test in case no other tests are present
+"""
+# Import Standard Libraries
+import pytest
+
+
+def test_passed():
+    """
+    The test is intended to be executed by GitHub Workflows in case no other tests are present
+
+    :return:
+        bool
+    """
+    assert True
